d03b399211a94c6a97d37580a2204bb8ee24b6a7
Lucciano Malap <luccianomalap@gmail.com>
2026-03-16 11:40:04 -0300

    subida act 2 y actualizada con parametros

--- Python-Basic-2.py
+++ Python-Basic-2.py
@@ -1,6 +1,6 @@
 
-def EsBisiesto():
-    year = int(input("Ingrese un numero de año: "))
+year = int(input("Ingrese un numero de año: "))
+def EsBisiesto(year):
     print(year % 4 == 0 or year % 400 == 0 and year % 100 != 0)
 
-EsBisiesto()
+EsBisiesto(year)
